4f71ea840d79c8aa4609ec682236a0e19a39adca
shemanisha <shetemanisha1509@gmail.com>
2025-08-30 22:18:30 +0530

    did backend setup and database setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const login = (req, res) => {
+    res.send("Login Route");
+}
+export const register = (req, res) => {
+    res.send("Register Route");
+}
+export const logout = (req, res) => {
+    res.send("Logout Route");
+}
